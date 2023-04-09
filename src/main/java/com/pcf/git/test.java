748556163df50a4b20c49fe481b74e7d9942a6bd
pcf <178431232@qq.com>
2023-04-09 09:19:28 +0800

    master commit

--- src/main/java/com/pcf/git/test.java
+++ src/main/java/com/pcf/git/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("你好，git1!");
         System.out.println("你好，git2");
+        System.out.println("你好，master");
     }
 }
